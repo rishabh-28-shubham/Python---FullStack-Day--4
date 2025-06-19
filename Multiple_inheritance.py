36e2d06acf9bc920d2508b72ea2adbf3988412c6
rishabh-28-shubham <ectopdash@gmail.com>
2025-06-19 10:43:38 +0530

    Multiple_inheritance

---
+++ Multiple_inheritance.py
@@ -0,0 +1,15 @@
+class A:
+    def method_A(self):
+        print("Method From A")
+
+class B:
+    def method_B(self):
+        print("Method From B")
+
+class C(A,B):
+    def method_C(self):
+        print("Method From C")
+
+test_obj = C()
+print(test_obj.method_A)
+test_obj.method_A()
